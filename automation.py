d666a407caa731f1614a27a2ac6fe7e79f067dc6
valluvarsploit <valluvarsploit@ValluvarSploit.valluvarsploit>
2020-12-22 12:45:51 +0530

    initial commit

---
+++ automation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+file1 = []
+file2 = []
+
+with open('domains.txt', 'r') as f1:
+    for line in f1:
+        file1.append(line.strip())
+
+f1.close()
+
+
+with open('domains2.txt', 'r') as f2:
+    for line in f2:
+        file2.append(line.strip())
+
+f2.close()
+
+for element in file1:
+    if element not in file2:
+        #print(element)
+        with open('output.txt', 'a') as output:
+            output.write(element)
+            output.write('\n')
+            
+
+output.close()
