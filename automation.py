0578a37f623f4fbbef2da3f4fff5350fc29c1a88
valluvarsploit <valluvarsploit@ValluvarSploit.valluvarsploit>
2020-12-22 15:40:42 +0530

    initial commit

--- automation.py
+++ automation.py
@@ -2,8 +2,8 @@
 
 domains_local_list = []
 domains_remote_list = []
-#wildcard_local_list = []
-#wildcard_remote_list = []
+wildcard_local_list = []
+wildcard_remote_list = []
 
 with open('domains.txt', 'r') as domains_remote_file:
     for line in domains_remote_file:
@@ -18,3 +18,18 @@ for element in domains_remote_list:
         with open('domains-output.txt', 'a') as domain_output:
             domain_output.write(element)
             domain_output.write('\n')
+
+
+with open('wildcards.txt', 'r') as wildcards_remote_file:
+    for line in wildcards_remote_file:
+        wildcard_remote_list.append(line.strip())
+
+with open('wildcard-local.txt', 'r') as wildcard_local_file:
+    for line in wildcard_local_file:
+        wildcard_local_list.append(line.strip())
+
+for element in wildcard_remote_list:
+    if element not in wildcard_local_list:
+        with open('wildcard-output.txt', 'a') as wildcard_output:
+            wildcard_output.write(element)
+            wildcard_output.write('\n')
