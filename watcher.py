4f08a851dfaa65e3cc05196cef45143595d1647e
ValluvarSploit <alexandar.thangavel@gmail.com>
2021-01-02 12:38:38 +0530

    bug fixs

---
+++ watcher.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import os
+
+os.system('touch domains-new.txt')
+os.system('touch wildcard-new.txt')
+
+domains_local_list = []
+domains_remote_list = []
+wildcards_local_list = []
+wildcards_remote_list = []
+
+with open('domains.txt', 'r') as domains_remote_file:
+    with open('wildcards.txt', 'r') as wildcards_remote_file:
+        with open('domains-local.txt', 'r') as domains_local_file:
+            with open('wildcard-local.txt', 'r') as wildcard_local_file:
+                for drline in domains_remote_file:
+                    domains_remote_list.append(drline.strip())
+                for wrline in wildcards_remote_file:
+                    wildcards_remote_list.append(wrline.strip())
+                for dlline in domains_local_file:
+                    domains_local_list.append(dlline.strip())
+                for wlline in wildcard_local_file:
+                    wildcards_local_list.append(wlline.strip())
+
+for element in domains_remote_list:
+    if element not in domains_local_list:
+        with open('domains-new.txt', 'a') as df:
+            df.write(element)
+            df.write('\n')
+
+for element in wildcards_remote_list:
+    if element not in wildcards_local_list:
+        with open('wildcard-new.txt', 'a') as wf:
+            wf.write(element)
+            wf.write('\n')
